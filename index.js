4fd3457d64176cb7a3fe32546e1bad94e2d9982b
Jai <jaijai.stell@gmail.com>
2023-03-24 12:18:10 -0700

    complete lab finally thanks cohort peers for help

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+//Jais code
+//To pass the tests, don't forget to return your fetch!
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books").then((resp) => resp.json()).then((json) => renderBooks(json));
+  //no need for return because no {}
+  //resp = response
 }
+//return fetchBooks();
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
